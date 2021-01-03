591d1b77e56b4055a9eaf0c063c19953e5a9fc96
Daylan <daylanberry@gmail.com>
2021-01-03 15:15:29 -0800

    creating jotto with tests

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme, { shallow } from 'enzyme';
+import App from './App';
+
+test('renders learn react link', () => {
+
+});
